79a8fd731ef1c143919361bff86b1f34709934c2
WaleedBinArshad <waleedshah10@gmail.com>
2018-02-20 15:31:39 +0100

    Update front-end codebase

--- WEB-INF/pages/js/controllers/home-controller.js
+++ WEB-INF/pages/js/controllers/home-controller.js
@@ -1,22 +1,30 @@
-app.controller('homeCtrl', function($scope, $http) {
-    $scope.getData = function(){
-        $http({
-            method : "GET",
-            url : "/data/urls.json"
-        }).then(function mySuccess(response) {
-            $scope.pendingURIs = response.data.urIs.pendingURIs;
-            $scope.nextCrawledURIs = response.data.urIs.nextCrawledURIs;
-            $scope.countOfWorker = response.data.urIs.countOfWorker;
-            $scope.countOfDeadWorker = response.data.urIs.countOfDeadWorker;
-            $scope.runtimeInSeconds = response.data.urIs.runtimeInSeconds;
-            $scope.writeTime = response.data.urIs.writeTime;
-            $scope.readTime = response.data.urIs.readTime;
-            $scope.ipStringListMap = response.data.urIs.ipStringListMap;
-            //console.log($scope.countOfWorker);
-        }, function myError(response) {
-            console.log(response);
-        });
+app.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
+  
+  $scope.autoUpdate = true;
+
+  $scope.getData = function() {
+    if ($scope.autoUpdate) {
+      $http({
+        method : "GET",
+        url : "/observer"
+      }).then(function mySuccess(response) {
+        $scope.pendingURIs = response.data.countOfPendingURIs;
+        $scope.countOfCrawledURIs = response.data.countOfCrawledURIs;
+        $scope.countOfWorker = response.data.countOfWorker;
+        $scope.countOfDeadWorker = response.data.countOfDeadWorker;
+        $scope.runtimeInSeconds = response.data.runtimeInSeconds;
+        $scope.writeTime = response.data.writeTime;
+        $scope.readTime = response.data.readTime;
+        $scope.ipStringListMap = response.data.ipStringListMap;
+        $scope.PendingURIS = response.data.pendingURIs;
+        $scope.NextCrawledURIs = response.data.nextCrawledURIs;
+      }, function myError(response) {
+        console.log(response);
+      });
     }
-    //Refreshed data after 10 seconds
-    setInterval($scope.getData, 50000);
-});
+  };
+
+  $scope.getData();
+
+  setInterval($scope.getData, 5000);
+}]);
