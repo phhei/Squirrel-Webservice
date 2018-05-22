51abce32d04ec562366e007c905492b8de56dd9e
WaleedBinArshad <waleedshah10@gmail.com>
2018-05-22 19:30:12 +0200

    Deleting Help Page. Adding new members in About Us Page. And History Slider stuff.

--- WEB-INF/pages/js/controllers/home-controller.js
+++ WEB-INF/pages/js/controllers/home-controller.js
@@ -1,23 +1,7 @@
 app.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.autoUpdate = true;
-  $scope.searchQuery = 'query';
-
-  $scope.searchQuery = function () {
-      $http({
-          method : "GET",
-          url : "/observer/inputValue"
-      }).then(function mySuccess(response) {
-          $scope.totalURIS = response.data.totalURIS;
-          $scope.runtimeInSeconds = response.data.runtimeInSeconds;
-          $scope.readTime = response.data.readTime;
-          $scope.writeTime = response.data.writeTime;
-          $scope.ipStringListMap = response.data.ipStringListMap;
-
-      }, function myError(response) {
-          console.log(response);
-      });
-  }
+  $scope.slider = 0;
 
 
   $scope.getData = function() {
@@ -36,19 +20,25 @@ app.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.ipStringListMap = response.data.ipStringListMap;
         $scope.PendingURIS = response.data.pendingURIs;
         $scope.NextCrawledURIs = response.data.nextCrawledURIs;
+        $scope.totalUrls = response.data.totalUrls;
       }, function myError(response) {
         console.log(response);
       });
     }
   };
 
-    $scope.get1 = function() {
-        console.log("get1 called.");
-    };
-
-    $scope.get1();
-    $scope.searchQuery();
     $scope.getData();
+    $scope.onSliderChange = function () {
+        $http({
+            method : "GET",
+            url : "/observer/stat?prop=lsc"
+        }).then(function mySuccess(response) {
+            $scope.getData();
+        }, function myError(response) {
+            console.log(response);
+
+        });
+    };
 
 
   setInterval($scope.getData, 5000);
